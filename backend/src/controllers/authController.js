94114a79fb6dac6343dee9320fb6d3f6e1d04556
Kartik Dimri <kartikdimri2005@gmail.com>
2026-07-19 00:15:13 +0530

    Fix production authentication cookies

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -83,9 +83,11 @@ export const loginUser = async (req, res) => {
 
     // Store JWT in Cookie
     res.cookie("token", token, {
-      httpOnly: true,
-    });
-
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+});
     res.status(200).json({
       message: "Login Successfully",
     });
@@ -97,8 +99,11 @@ export const loginUser = async (req, res) => {
 };
 
 export const logoutUser = (req, res) => {
-  res.clearCookie("token");
-
+  res.clearCookie("token", {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+});
   res.status(200).json({
     message: "Logged out successfully",
   });
